408a5f26cc92bbc4a423e06d21f9d5f13dd59ae1
githsem <53379599+githsem@users.noreply.github.com>
2020-10-03 01:46:21 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -3,3 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Hallo Welt/Documents/GitHub/Todo_App/todo.db'
 db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
