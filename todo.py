254d1496538d5f36d47798e203aa0cd0e67e6083
githsem <53379599+githsem@users.noreply.github.com>
2020-10-03 01:43:40 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////C:\Users\Hallo Welt\Documents\GitHub\Todo_App'
 db = SQLAlchemy(app)
