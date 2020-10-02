ab0b57d34ba8915fa3f4a0141ede6b60cc147521
githsem <53379599+githsem@users.noreply.github.com>
2020-10-03 01:47:45 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -6,5 +6,4 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    title = db.Column(db.String(80))
