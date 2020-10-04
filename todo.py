16816d9a980740a0ed9b7a6014e0ef48c73f045c
githsem <53379599+githsem@users.noreply.github.com>
2020-10-04 23:16:11 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template, render_template, url_for
+from flask import Flask, render_template, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Hallo Welt/Documents/GitHub/Todo_App/todo.db'
 db = SQLAlchemy(app)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    todos = Todo.query.all()
+    return render_template("index.html", todos=todos)
+
+
+@app.route("/complete/<string:id>")
+def completeTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complet = not todo.complet
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/add", methods=["POST"])
+def addTodo():
+    title = request.form.get("title")
+    newTodo = Todo(title=title, complet=False)
+    db.session.add(newTodo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<string:id>")
+def deleteTodo(id):
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,4 +47,4 @@ class Todo(db.Model):
 
 if __name__ == "__main__":
     db.create_all()
-    app.run(debug = True)   
+    app.run(debug=True)
